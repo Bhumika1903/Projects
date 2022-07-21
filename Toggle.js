69744fa52de44c9bb521af27fa51b0c6f502b80e
Bhumika Namdeo <82503987+Bhumika1903@users.noreply.github.com>
2022-07-21 14:38:40 +0530

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,7 @@
+var toggle = document.getElementById('onclick');
+var heading = document.getElementById('heading');
+toggle.addEventListener('click' , function(){
+	document.body.style.background = 'black';
+	heading.style.color = 'white';
+
+})
